b61ecc39c922b6808c62e6cece81b4cff897709e
Thinh Nguyen <tienthinh2011@gmail.com>
2018-10-27 15:24:36 +0700

    [K64FNet][Code][LWIP] remove old user_apps folder
    [IssueType] Configuration
    [Task] LWIP Bringup
    [Detail][0] Remove old user_apps folder

--- user_apps/lwip/KSDK_Network_Demo/network_demo_ucosiii/network_demo/kds/debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := C:/Freescale/KSDK_1.3.0/examples/K6xNetworkDemo/user_apps/lwip/KSDK_Network_Demo/network_demo_ucosiii/network_demo/kds/../../../../../../../../lib/ksdk_ucosiii_lib/kds/K64F12/debug/libksdk_platform_ucosiii.a
-
-LIBS := -lm -lg -lgcc -lnosys
-
